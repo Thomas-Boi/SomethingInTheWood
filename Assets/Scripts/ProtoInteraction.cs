1b2823ebdb969216e487d551281cc5ce1534a3f7
Sebastian Bejm <125sbejm@gmail.com>
2021-02-25 15:39:46 -0800

    Added test scene for interacting

---
+++ Assets/Scripts/ProtoInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoInteraction : MonoBehaviour
+{
+
+    [SerializeField]
+    private float interactRange = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If object is in specific range of player, allow to interact
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+
+    private bool ObjectInRange()
+    {
+
+        return false;
+    }
+
+    private void StartInteraction()
+    {
+
+    }
+}
