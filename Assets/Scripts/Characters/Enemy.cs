b00c6012fc1ca7ea887c85aa5f3a3d57314e9b56
JasonZFHuang <jasonhuangzf@gmail.com>
2021-03-11 16:50:04 -0800

    boar ai

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -9,10 +9,14 @@ public class Enemy : MonoBehaviour
     [SerializeField] Transform target;
 
     private NavMeshAgent agent;
+    private Animator anim;
+    private SpriteRenderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
@@ -23,5 +27,20 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         agent.SetDestination(target.position);
+
+        PlayAnimation();
+        
+    }
+
+    // todo: Move this to a subclass
+    void PlayAnimation() {
+        anim.speed = Mathf.Abs(agent.velocity.x)/10;
+        
+        if (agent.velocity.x < 0) {
+            rend.flipX = true;
+        } else {
+            rend.flipX = false;
+        }
+
     }
 }
