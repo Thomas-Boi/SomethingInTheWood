b33c2048803fe1c3e842b07dc94b685153e0e12d
Sebastian Bejm <125sbejm@gmail.com>
2021-03-25 14:09:04 -0700

    Added health bar to UI

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Text hpText;
+    //public bool isEnemy;
+
+    void Start()
+    {
+        /*if (isEnemy)
+        {
+            hpText.gameObject.SetActive(false);
+        }*/
+    }
+
+    void Update()
+    {
+        /*if (!isEnemy)
+        {
+            UpdateHPString();
+        }*/
+        UpdateHPString();
+    }
+
+    private void UpdateHPString()
+    {
+        int curHp = (int)slider.value;
+        int maxHp = (int)slider.maxValue;
+        string hpStr = curHp + " / " + maxHp;
+        hpText.text = hpStr;
+    }
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+}
